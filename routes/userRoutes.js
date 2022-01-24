3995eb25138b3c913c330135faf1d16bf8bc8eb2
withewolf-dev <itskashyap26@gmail.com>
2022-01-24 18:29:25 +0530

    access chat

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,9 +4,11 @@ const {
   authUser,
   allUsers,
 } = require("../controller/userController");
+const { protect } = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.route("/").post(registerUser);
 router.post("/login", authUser);
-router.get("/", allUsers);
+router.get("/", protect, allUsers);
+
 module.exports = router;
