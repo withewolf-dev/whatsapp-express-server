702d7e3390a46c230c3f95543257ad53736c8081
withewolf-dev <itskashyap26@gmail.com>
2022-02-07 15:07:16 +0530

    updated user model

--- models/userModel.js
+++ models/userModel.js
@@ -1,17 +1,15 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const userSchema = mongoose.Schema(
   {
-    name: { type: "String", required: true },
-    email: { type: "String", unique: true, required: true },
-    password: { type: "String", required: true },
+    name: { type: 'String', required: true },
+    email: { type: 'String', unique: true, required: true },
+    password: { type: 'String', required: true },
     pic: {
-      type: "String",
-      required: true,
-      default:
-        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
-    },
+      type: 'String',
+      required: true
+    }
     //   isAdmin: {
     //     type: Boolean,
     //     required: true,
@@ -19,21 +17,21 @@ const userSchema = mongoose.Schema(
     //   },
   },
   { timestaps: true }
-);
+)
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
+  return await bcrypt.compare(enteredPassword, this.password)
+}
 
-userSchema.pre("save", async function (next) {
+userSchema.pre('save', async function (next) {
   if (!this.isModified) {
-    next();
+    next()
   }
 
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-});
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema)
 
-module.exports = User;
+module.exports = User
